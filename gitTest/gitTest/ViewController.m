497a8db9987a73977e57f153d1c96644edfc4ef9
cheng <cheng@163.com>
2016-08-26 16:37:37 +0800

    张三开始开发了

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"我是新来的");
+    NSLog(@"我是员工,开始干活了");
 }
 
 - (void)didReceiveMemoryWarning {
